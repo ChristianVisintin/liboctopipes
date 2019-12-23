b410bc82518881628235580dbde4d96dd95394d9
ChristianVisintin <christian.visintin1997@gmail.com>
2019-12-23 09:18:10 +0100

    octopipes clients

--- libs/liboctopipes/clients/octopipes_send.c
+++ libs/liboctopipes/clients/octopipes_send.c
@@ -21,6 +21,116 @@
  * SOFTWARE.
 **/
 
+#include <octopipes/octopipes.h>
+
+#include <getopt.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#define DEFAULT_CAP_PATH "/usr/share/octopipes/pipes/cap.fifo"
+#define DEFAULT_CLIENT_ID "octopipes-client"
+#define DEFAULT_PROTOCOL_VERSION OCTOPIPES_VERSION_1
+
+#define PROGRAM_NAME "octopipes_send"
+#define USAGE PROGRAM_NAME " built against liboctopipes " OCTOPIPES_LIB_VERSION "\n\
+Usage: " PROGRAM_NAME " [options...] payload\n\
+\t-C <CAP path>\t\tSpecify the Common Access Pipe path\n\
+\t-r <remote>\t\tSpeicify the remote (or group) to send the payload to\n\
+\t-i <client id>\t\tSpeicify the client ID\n\
+\t-V <protocol version>\tSpecify the protocol version to use (Default: 1)\n\
+\t-v\t\t\tVerbose\n\
+\t-h\t\t\tShow this page\n\
+"
+
+//Options
+char* cap_path = DEFAULT_CAP_PATH;
+char* remote = NULL;
+char* client_id = DEFAULT_CLIENT_ID;
+char* payload = NULL;
+int protocol_version = DEFAULT_PROTOCOL_VERSION;
+bool sigterm_called = false;
+bool verbose = false;
+
 int main(int argc, char** argv) {
-  return 0;
+  int opt;
+  //Getopt
+  while ((opt = getopt(argc, argv, "C:r:i:V:vh")) != -1) {
+    switch (opt) {
+    case 'C':
+      cap_path = optarg;
+      break;
+    case 'r':
+      remote = optarg;
+      break;
+    case 'i':
+      client_id = optarg;
+      break;
+    case 'V':
+      protocol_version = atoi(optarg);
+      break;
+    case 'v':
+      verbose = true;
+      break;
+    case 'h':
+      printf("%s\n", USAGE);
+      return 0;
+    }
+  }
+
+  if (optind < argc) {
+    payload = argv[optind];
+  } else {
+    printf("Missing payload\n");
+    return 1;
+  }
+
+  if (verbose) {
+    printf("CAP Path: %s\n", cap_path);
+    printf("Client ID: %s\n", client_id);
+    printf("Payload: %s\n", payload);
+    printf("Protocol Version: %d\n", protocol_version);
+    printf("Remote: %s\n", remote);
+    printf("Verbose: %d\n", verbose);
+  }
+
+  //Initialize client
+  OctopipesClient* client;
+  OctopipesError rc;
+  OctopipesCapError assignment_error;
+  if ((rc = octopipes_init(&client, client_id, cap_path, protocol_version)) != OCTOPIPES_ERROR_SUCCESS) {
+    printf("Could not initialize OctopipesClient: %s\n", octopipes_get_error_desc(rc));
+    client = NULL;
+    goto exit;
+  }
+  if (verbose) {
+    printf("Client initialized\n");
+  }
+
+  //Subscribe
+  if ((rc = octopipes_subscribe(client, NULL, 0, &assignment_error)) != OCTOPIPES_ERROR_SUCCESS) {
+    printf("Could not subscribe OctopipesClient: %s (CAP Error: %d)\n", octopipes_get_error_desc(rc), assignment_error);
+    goto exit;
+  }
+  if (verbose) {
+    printf("Successfully subscribed to Octopipes Server\n");
+  }
+
+  //Send message
+  if ((rc = octopipes_send(client, remote, (void*) payload, strlen(payload))) != OCTOPIPES_ERROR_SUCCESS) {
+    printf("Could not send message to Octopipes Server: %s\n", octopipes_get_error_desc(rc));
+    goto exit;
+  }
+
+  //Unsubscribe
+  if ((rc = octopipes_unsubscribe(client)) != OCTOPIPES_ERROR_SUCCESS) {
+    printf("Could not unsubscribe loop: %s\n", octopipes_get_error_desc(rc));
+    goto exit;
+  }
+
+exit:
+  //Free client
+  octopipes_cleanup(client);
+  return rc;
 }
--- libs/liboctopipes/clients/octopipes_sub.c
+++ libs/liboctopipes/clients/octopipes_sub.c
@@ -21,6 +21,193 @@
  * SOFTWARE.
 **/
 
+#include <octopipes/octopipes.h>
+
+#include <getopt.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#define DEFAULT_CAP_PATH "/usr/share/octopipes/pipes/cap.fifo"
+#define DEFAULT_CLIENT_ID "octopipes-client"
+#define DEFAULT_PROTOCOL_VERSION OCTOPIPES_VERSION_1
+
+#define PROGRAM_NAME "octopipes_sub"
+#define USAGE PROGRAM_NAME " built against liboctopipes " OCTOPIPES_LIB_VERSION "\n\
+Usage: " PROGRAM_NAME " [options...] subscriptions\n\
+\t-C <CAP path>\t\tSpecify the Common Access Pipe path\n\
+\t-c <count>\t\tIndicates the amount of messages to receive before exiting\n\
+\t-i <client id>\t\tSpeicify the client ID\n\
+\t-V <protocol version>\tSpecify the protocol version to use (Default: 1)\n\
+\t-v\t\t\tVerbose\n\
+\t-h\t\t\tShow this page\n\
+"
+
+//Options
+char** groups = NULL;
+size_t groups_count = 0;
+int count = -1;
+char* cap_path = DEFAULT_CAP_PATH;
+char* client_id = DEFAULT_CLIENT_ID;
+int protocol_version = DEFAULT_PROTOCOL_VERSION;
+bool sigterm_called = false;
+bool verbose = false;
+
+int message_counter = 0;
+
+/**
+ * @brief on message received callback
+ * @param OctopipesMessage*
+ */
+
+void on_received(const OctopipesMessage* message) {
+  message_counter++;
+  if (message_counter >= count && count != -1) {
+    return;
+  }
+  if (message->data_size > 0 && message->data != NULL) {
+    if (verbose) {
+      printf("%s %s\n", message->origin, message->data);
+    } else {
+      printf("%s\n", message->data);
+    }
+  } else {
+    if (verbose) {
+      printf("%s\n", message->origin);
+    }
+  }
+}
+
+/**
+ * @brief on error raised callback
+ * @param OctopipesError
+ */
+
+void on_error(const OctopipesError error) {
+  printf("ERROR: %s\n", octopipes_get_error_desc(error));
+}
+
+/**
+ * @function handleSigterm
+ * @description: terminate main loop in order to terminate einklient
+ * @param int
+**/
+
+void handleSigterm(int s) {
+  sigterm_called = true;
+}
+
 int main(int argc, char** argv) {
-  return 0;
+  int opt;
+  //Getopt
+  while ((opt = getopt(argc, argv, "C:c:i:V:vh")) != -1) {
+    switch (opt) {
+    case 'C':
+      cap_path = optarg;
+      break;
+    case 'c':
+      count = atoi(optarg);
+      break;
+    case 'i':
+      client_id = optarg;
+      break;
+    case 'V':
+      protocol_version = atoi(optarg);
+      break;
+    case 'v':
+      verbose = true;
+      break;
+    case 'h':
+      printf("%s\n", USAGE);
+      return 0;
+    }
+  }
+
+  groups_count = (argc - optind - 1);
+  //Allocate groups
+  groups = (char**) malloc(sizeof(char**) * groups_count);
+  if (groups == NULL) {
+    printf("Could not allocate more memory for groups container\n");
+    return 1;
+  }
+  //Get subscribptions
+  for (size_t i; optind < argc; optind++) {
+    groups[i++] = argv[optind]; //Won't be freed since not allocated dynamically
+  }
+
+  if (verbose) {
+    printf("CAP Path: %s\n", cap_path);
+    printf("Count: %d\n", count);
+    printf("Client ID: %s\n", client_id);
+    printf("Protocol Version: %d\n", protocol_version);
+    printf("Verbose: %d\n", verbose);
+    for (size_t i = 0; i < groups_count; i++) {
+      printf("Group: %s\n", groups[i]);
+    }
+  }
+
+  //SIGTERM handler
+  struct sigaction sigTermHandler;
+  sigTermHandler.sa_handler = handleSigterm;
+  sigemptyset(&sigTermHandler.sa_mask);
+  sigTermHandler.sa_flags = 0;
+  sigaction(SIGTERM, &sigTermHandler, NULL);
+  sigaction(SIGINT, &sigTermHandler, NULL);
+
+  //Initialize client
+  OctopipesClient* client;
+  OctopipesError rc;
+  OctopipesCapError assignment_error;
+  if ((rc = octopipes_init(&client, client_id, cap_path, protocol_version)) != OCTOPIPES_ERROR_SUCCESS) {
+    printf("Could not initialize OctopipesClient: %s\n", octopipes_get_error_desc(rc));
+    client = NULL;
+    goto exit;
+  }
+  if (verbose) {
+    printf("Client initialized\n");
+  }
+
+  //Set callback
+  octopipes_set_received_cb(client, on_received);
+  octopipes_set_receive_error_cb(client, on_error);
+
+  //Subscribe
+  if ((rc = octopipes_subscribe(client, (const char**) groups, groups_count, &assignment_error)) != OCTOPIPES_ERROR_SUCCESS) {
+    printf("Could not subscribe OctopipesClient: %s (CAP Error: %d)\n", octopipes_get_error_desc(rc), assignment_error);
+    goto exit;
+  }
+  if (verbose) {
+    printf("Successfully subscribed to Octopipes Server\n");
+  }
+  //Start loop
+  if ((rc = octopipes_loop_start(client)) != OCTOPIPES_ERROR_SUCCESS) {
+    printf("Could not start loop: %s\n", octopipes_get_error_desc(rc));
+    goto exit;
+  }
+  if (verbose) {
+    printf("Listening for incoming messages\n");
+  }
+
+  while (!sigterm_called && (message_counter < count || count == -1)) {
+    sleep(1); //Loops
+  }
+
+  //Unsubscribe
+  if ((rc = octopipes_unsubscribe(client)) != OCTOPIPES_ERROR_SUCCESS) {
+    printf("Could not unsubscribe loop: %s\n", octopipes_get_error_desc(rc));
+    goto exit;
+  }
+
+  //Stop loop
+  if ((rc = octopipes_loop_stop(client)) != OCTOPIPES_ERROR_SUCCESS) {
+    printf("Could not stop loop: %s\n", octopipes_get_error_desc(rc));
+    goto exit;
+  }
+
+exit:
+  //Free groups
+  free(groups);
+  //Free client
+  octopipes_cleanup(client);
+  return rc;
 }
