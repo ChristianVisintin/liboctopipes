c530fab5c1dd628532cc8e0572a22b1bc187d7c9
ChristianVisintin <christian.visintin1997@gmail.com>
2019-12-31 17:53:32 +0100

    Pub stop/start worker

--- libs/liboctopipes/clients/octopipes_send.c
+++ libs/liboctopipes/clients/octopipes_send.c
@@ -29,7 +29,7 @@
 #include <string.h>
 
 #define DEFAULT_CAP_PATH "/usr/share/octopipes/pipes/cap.fifo"
-#define DEFAULT_CLIENT_ID "octopipes-client"
+#define DEFAULT_CLIENT_ID "octopipes-client-"
 #define DEFAULT_PROTOCOL_VERSION OCTOPIPES_VERSION_1
 
 #define PROGRAM_NAME "octopipes_send"
@@ -46,11 +46,43 @@ Usage: " PROGRAM_NAME " [options...] payload\n\
 //Options
 char* cap_path = DEFAULT_CAP_PATH;
 char* remote = NULL;
-char* client_id = DEFAULT_CLIENT_ID;
+char* client_id = NULL;
 char* payload = NULL;
 int protocol_version = DEFAULT_PROTOCOL_VERSION;
 bool sigterm_called = false;
 bool verbose = false;
+
+/**
+ * @brief generate a random string of alphanumerical characters
+ * @param char* str
+ * @param size_t size of string
+ * @return char*
+ */
+
+static char* gen_rand_string(char* str, size_t size) {
+  const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  if (size) {
+    --size;
+    for (size_t n = 0; n < size; n++) {
+      int key = rand() % (int) (sizeof(charset) - 1);
+      str[n] = charset[key];
+    }
+    str[size] = '\0';
+  }
+  return str;
+}
+
+/**
+ * @brief generates a random client id
+ * @return char*
+ */
+
+char* gen_random_clid() {
+  char* clid = (char*) malloc(sizeof(char) * 26); //octopipes-client + 8 random bytes + 0x00
+  memcpy(clid, DEFAULT_CLIENT_ID, 17);
+  gen_rand_string(clid + 17, 8);
+  return clid;
+}
 
 int main(int argc, char** argv) {
   int opt;
@@ -83,6 +115,17 @@ int main(int argc, char** argv) {
   } else {
     printf("Missing payload\n");
     return 1;
+  }
+
+  if (remote == NULL) {
+    printf("Missing remote\n");
+    return 1;
+  }
+
+  char* to_free_clid = NULL;
+  if (client_id == NULL) {
+    client_id = gen_random_clid();
+    to_free_clid = client_id;
   }
 
   if (verbose) {
@@ -131,5 +174,8 @@ int main(int argc, char** argv) {
 exit:
   //Free client
   octopipes_cleanup(client);
+  if (to_free_clid != NULL) {
+    free(to_free_clid);
+  }
   return rc;
 }
--- libs/liboctopipes/clients/octopipes_sub.c
+++ libs/liboctopipes/clients/octopipes_sub.c
@@ -26,6 +26,7 @@
 #include <getopt.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #ifndef _WIN32
@@ -33,7 +34,7 @@
 #endif
 
 #define DEFAULT_CAP_PATH "/usr/share/octopipes/pipes/cap.fifo"
-#define DEFAULT_CLIENT_ID "octopipes-client"
+#define DEFAULT_CLIENT_ID "octopipes-client-"
 #define DEFAULT_PROTOCOL_VERSION OCTOPIPES_VERSION_1
 
 #define PROGRAM_NAME "octopipes_sub"
@@ -52,7 +53,7 @@ char** groups = NULL;
 size_t groups_count = 0;
 int count = -1;
 char* cap_path = DEFAULT_CAP_PATH;
-char* client_id = DEFAULT_CLIENT_ID;
+char* client_id = NULL;
 int protocol_version = DEFAULT_PROTOCOL_VERSION;
 bool sigterm_called = false;
 bool verbose = false;
@@ -105,6 +106,38 @@ void handleSigterm(int s) {
 
 #endif
 
+/**
+ * @brief generate a random string of alphanumerical characters
+ * @param char* str
+ * @param size_t size of string
+ * @return char*
+ */
+
+static char* gen_rand_string(char* str, size_t size) {
+  const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  if (size) {
+    --size;
+    for (size_t n = 0; n < size; n++) {
+      int key = rand() % (int) (sizeof(charset) - 1);
+      str[n] = charset[key];
+    }
+    str[size] = '\0';
+  }
+  return str;
+}
+
+/**
+ * @brief generates a random client id
+ * @return char*
+ */
+
+char* gen_random_clid() {
+  char* clid = (char*) malloc(sizeof(char) * 26); //octopipes-client + 8 random bytes + 0x00
+  memcpy(clid, DEFAULT_CLIENT_ID, 17);
+  gen_rand_string(clid + 17, 8);
+  return clid;
+}
+
 int main(int argc, char** argv) {
   int opt;
   //Getopt
@@ -129,6 +162,11 @@ int main(int argc, char** argv) {
       printf("%s\n", USAGE);
       return 0;
     }
+  }
+  char* to_free_clid = NULL;
+  if (client_id == NULL) {
+    client_id = gen_random_clid();
+    to_free_clid = client_id;
   }
 
   groups_count = (argc - optind - 1) < 0 ? 0 : (argc - optind - 1);
@@ -221,5 +259,8 @@ exit:
   free(groups);
   //Free client
   octopipes_cleanup(client);
+  if (to_free_clid != NULL) {
+    free(to_free_clid);
+  }
   return rc;
 }
